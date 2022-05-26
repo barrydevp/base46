a53229c8967b9b09f26a8029b082596af2c1a159
barrydevp <barrydevp@gmail.com>
2022-05-26 20:40:07 +0700

    update cursor color

--- lua/integrations/misc.lua
+++ lua/integrations/misc.lua
@@ -161,10 +161,10 @@ return {
       sp = "none",
    },
 
-   StatusLineNC = {
-      bg = "NONE",
-      sp = "none",
-   },
+   -- StatusLineNC = {
+   --    bg = "NONE",
+   --    sp = "none",
+   -- },
 
    -- spell
 
