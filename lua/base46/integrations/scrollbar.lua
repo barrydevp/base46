753cf3f50fa3cdf5a7c3d7aa73461b9b922d28c2
barrydevp <barrydevp@gmail.com>
2022-11-26 20:22:17 +0700

    chore: add scrollbar integration

--- lua/base46/integrations/scrollbar.lua
+++ lua/base46/integrations/scrollbar.lua
@@ -2,26 +2,26 @@ local colors = require("base46").get_theme_tb "base_30"
 
 -- Scrollbar
 return {
-    ScrollbarHandle = { fg = colors.none, bg = colors.white },
+    ScrollbarHandle = { fg = colors.none, bg = colors.light_gray },
 
-    ScrollbarCursorHandle = { fg = colors.blue, bg = colors.white },
-    ScrollbarCursor = { fg = colors.blue, bg = colors.none },
+    ScrollbarCursorHandle = { fg = colors.white, bg = colors.light_gray },
+    ScrollbarCursor = { fg = colors.white, bg = colors.none },
 
-    ScrollbarSearchHandle = { fg = colors.orange, bg = colors.white },
+    ScrollbarSearchHandle = { fg = colors.orange, bg = colors.light_gray },
     ScrollbarSearch = { fg = colors.orange, bg = colors.none },
 
-    ScrollbarErrorHandle = { fg = colors.error, bg = colors.white },
+    ScrollbarErrorHandle = { fg = colors.error, bg = colors.light_gray },
     ScrollbarError = { fg = colors.error, bg = colors.none },
 
-    ScrollbarWarnHandle = { fg = colors.warning, bg = colors.white },
+    ScrollbarWarnHandle = { fg = colors.warning, bg = colors.light_gray },
     ScrollbarWarn = { fg = colors.warning, bg = colors.none },
 
-    ScrollbarInfoHandle = { fg = colors.info, bg = colors.white },
+    ScrollbarInfoHandle = { fg = colors.info, bg = colors.light_gray },
     ScrollbarInfo = { fg = colors.info, bg = colors.none },
 
-    ScrollbarHintHandle = { fg = colors.hint, bg = colors.white },
+    ScrollbarHintHandle = { fg = colors.hint, bg = colors.light_gray },
     ScrollbarHint = { fg = colors.hint, bg = colors.none },
 
-    ScrollbarMiscHandle = { fg = colors.purple, bg = colors.white },
+    ScrollbarMiscHandle = { fg = colors.purple, bg = colors.light_gray },
     ScrollbarMisc = { fg = colors.purple, bg = colors.none },
 }
