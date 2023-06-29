97a592a670ef75c511e887c45a3ad6255625751a
barrydevp <barrydevp@gmail.com>
2023-06-30 01:51:57 +0700

    update default highlight group

---
+++ lua/base46/integrations/ufo.lua
@@ -0,0 +1,13 @@
+local colors = require("base46").get_theme_tb "base_30"
+
+-- nvim-ufo
+return {
+  -- UfoFoldedFg = { fg = colors.none, bg = colors.one_bg3 },
+  -- UfoFoldedBg = { fg = colors.light_grey, bg = colors.one_bg3 },
+  -- UfoPreviewSbar = { fg = colors.white, bg = colors.none },
+  -- UfoPreviewThumb = { fg = colors.orange, bg = colors.one_bg3 },
+  -- UfoPreviewWinBar = { fg = colors.orange, bg = colors.none },
+  -- UfoPreviewCursorLine = { fg = colors.error, bg = colors.one_bg3 },
+  -- UfoFoldedEllipsis = { fg = colors.error, bg = colors.none },
+  -- UfoCursorFoldedLine = { fg = colors.warning, bg = colors.one_bg3 },
+}
